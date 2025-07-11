68b8ce78d8f3bb53b7a5bbed7ad5b0ba29193ffa
Tom Peham <pehamtom@gmx.at>
2025-07-11 08:57:42 +0200

    Actually return draw object.

--- src/mqt/qecc/circuit_synthesis/circuits.py
+++ src/mqt/qecc/circuit_synthesis/circuits.py
@@ -268,14 +268,14 @@ class CNOTCircuit:
         """
         return [qubit for qubit, basis in self.initializations.items() if basis.upper() == "Z"]
 
-    def draw(self, *args, **kwargs) -> None:  # type: ignore[no-untyped-def] # noqa: ANN003, ANN002
+    def draw(self, *args, **kwargs):  # noqa: ANN003, ANN002, ANN201
         """Draw the circuit using Qiskit visualization tools.
 
         Args:
             *args: Positional arguments for the Qiskit draw method.
             **kwargs: Keyword arguments for the Qiskit draw method.
         """
-        self.to_qiskit_circuit().draw(*args, **kwargs)
+        return self.to_qiskit_circuit().draw(*args, **kwargs)
 
     def get_code(self) -> CSSCode:
         """Get CSS code defined by the circuit.
