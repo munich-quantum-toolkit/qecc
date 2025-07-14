838331f7f44df96fbd4488be6a24e909937cd984
LSHerzog <laura.s.herzog@hotmail.de>
2025-07-14 09:59:37 +0200

    mypy errors in test_misc

--- test/python/co3/test_misc.py
+++ test/python/co3/test_misc.py
@@ -18,10 +18,15 @@ def _split_layers_cnot(circuit: list[tuple[int, int] | int]) -> list[list[tuple[
     The input can also have mere ints, but this is only for mypy. This helper function only assumes tuple[int,int] in the circuit.
     """
     result = []
-    current_group = []
-    seen = set()
+    current_group: list[tuple[int,int] | int] = []
+    seen: set[int] = set()
 
-    for a, b in circuit:
+    for tup in circuit:
+        if isinstance(tup, tuple):
+            a, b = tup
+        else:
+            msg = "This function can handle only pure cnot circuits."
+            raise TypeError(msg)
         if a in seen or b in seen:
             result.append(current_group)
             current_group = []
