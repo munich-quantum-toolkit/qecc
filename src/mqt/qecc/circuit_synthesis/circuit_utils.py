c17f234d3a483c31457aabbd21bd058251ce4fcc
Tom Peham <pehamtom@gmx.at>
2025-07-24 12:57:43 +0200

    Removed unused method

--- src/mqt/qecc/circuit_synthesis/circuit_utils.py
+++ src/mqt/qecc/circuit_synthesis/circuit_utils.py
@@ -5,43 +5,13 @@
 #
 # Licensed under the MIT License
 
-"""General circuit constructions."""
+"""Utility methods for circuits."""
 
 from __future__ import annotations
 
-from qiskit.circuit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit
 from qiskit.converters import circuit_to_dag, dag_to_circuit
 from stim import Circuit
-
-
-def reorder_qubits(circ: QuantumCircuit, qubit_mapping: dict[int, int]) -> QuantumCircuit:
-    """Reorders the qubits in a QuantumCircuit based on the given mapping.
-
-    Parameters:
-        circuit: The original quantum circuit.
-        qubit_mapping: A dictionary mapping original qubit indices to new qubit indices.
-
-    Returns:
-        A new quantum circuit with qubits reordered.
-    """
-    # Validate the qubit_mapping
-    if sorted(qubit_mapping.keys()) != list(range(len(circ.qubits))) or sorted(qubit_mapping.values()) != list(
-        range(len(circ.qubits))
-    ):
-        msg = "Invalid qubit_mapping: It must be a permutation of the original qubit indices."
-        raise ValueError(msg)
-
-    # Create a new quantum register
-    num_qubits = len(circ.qubits)
-    new_register = QuantumRegister(num_qubits, "q")
-    new_circuit = QuantumCircuit(new_register)
-
-    # Remap instructions based on the qubit_mapping
-    for instruction, qubits, clbits in circ.data:
-        new_qubits = [new_register[qubit_mapping[circ.find_bit(q)[0]]] for q in qubits]
-        new_circuit.append(instruction, new_qubits, clbits)
-
-    return new_circuit
 
 
 def relabel_qubits(circ: Circuit, qubit_mapping: dict[int, int] | int) -> Circuit:
