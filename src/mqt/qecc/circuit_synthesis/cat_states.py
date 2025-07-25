32ab2241dbaab3131ba1ce0cab2f6d47fa4891e6
Tom Peham <pehamtom@gmx.at>
2025-07-25 16:32:21 +0200

    Don't convert to qiskit in Cat States.

--- src/mqt/qecc/circuit_synthesis/cat_states.py
+++ src/mqt/qecc/circuit_synthesis/cat_states.py
@@ -14,9 +14,8 @@ from typing import TYPE_CHECKING
 import matplotlib.pyplot as plt
 import numpy as np
 import stim
-from qiskit import QuantumCircuit
 
-from .circuit_utils import qiskit_to_stim_circuit, relabel_qubits
+from .circuit_utils import relabel_qubits
 from .noise import CircuitLevelNoise
 
 if TYPE_CHECKING:
@@ -95,7 +94,6 @@ class CatStatePreparationExperiment:
         self.w = circ1.num_qubits
         self.circ = transversal_cnot(circ1, relabel_qubits(circ2, self.w), permutation)
         self.circ.append("MR", range(self.w, self.w * 2))
-        self.circ = qiskit_to_stim_circuit(QuantumCircuit.from_qasm_str(self.circ.to_qasm(open_qasm_version=2)))
 
     def _get_noisy_circ(self, p: float) -> stim.Circuit:
         """Return a noisy version of the cat state preparation circuit.
