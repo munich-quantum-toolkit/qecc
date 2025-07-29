e5cf48d19f9763a32356515783f03c976de8ce61
Ludwig Schmid <ludwig.s.schmid@tum.de>
2025-07-29 19:23:40 +0200

    added class for rotated surface codes

---
+++ src/mqt/qecc/codes/rotated_surface_code.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""A rotated surface code class."""
+
+from __future__ import annotations
+
+import numpy as np
+
+from .css_code import CSSCode
+
+
+class InvalidDistanceError(ValueError):
+    """Custom error for invalid distance in rotated surface code."""
+
+    def __init__(self, message: str) -> None:
+        """Initialize the InvalidDistanceError."""
+        super().__init__(message)
+        self.message = message
+
+
+class RotatedSurfaceCode(CSSCode):
+    """A rotated surface code class."""
+
+    def __init__(
+        self, distance: int | None = None, x_distance: int | None = None, z_distance: int | None = None
+    ) -> None:
+        """Initialize the rotated surface code."""
+        if distance is not None:
+            if distance % 2 == 0:
+                msg = "Distance must be odd."
+                raise ValueError(msg)
+            super().__init__(
+                self._generate_h("x", distance, distance), self._generate_h("z", distance, distance), distance
+            )
+        elif x_distance is None or z_distance is None:
+            msg = "Either distance or both x_distance and z_distance must be provided."
+            raise ValueError(msg)
+        else:
+            if x_distance % 2 == 0 or z_distance % 2 == 0:
+                msg = "x_distance and z_distance must be odd."
+                raise ValueError(msg)
+            super().__init__(
+                self._generate_h("x", x_distance, z_distance),
+                self._generate_h("z", x_distance, z_distance),
+                x_distance=x_distance,
+                z_distance=z_distance,
+            )
+
+    @staticmethod
+    def _generate_h(stab_type: str, x_distance: int, z_distance: int) -> np.ndarray:
+        """Generate the check matrix for the rotated surface code."""
+        if stab_type not in {"x", "z"}:
+            msg = "Type must be either 'x' or 'z'."
+            raise ValueError(msg)
+
+        n = x_distance * z_distance
+        h = np.empty((0, n), dtype=np.int8)
+
+        # squares
+        for i in range(x_distance - 1):
+            for j in range(z_distance - 1):
+                if (stab_type == "x" and (i + j) % 2 == 0) or (stab_type == "z" and (i + j) % 2 == 1):
+                    stab = np.zeros(n, dtype=np.int8)
+                    stab[i + j * x_distance] = 1
+                    stab[i + j * x_distance + 1] = 1
+                    stab[i + j * x_distance + x_distance] = 1
+                    stab[i + j * x_distance + x_distance + 1] = 1
+                    h = np.vstack((h, stab), dtype=np.int8)
+        # boundaries
+        if stab_type == "x":
+            for i in range(z_distance - 1):  # rows
+                stab = np.zeros(n, dtype=np.int8)
+                if i % 2 == 0:
+                    stab[i * x_distance + x_distance - 1] = 1
+                    stab[(i + 1) * x_distance + x_distance - 1] = 1
+                else:
+                    stab[i * x_distance] = 1
+                    stab[(i + 1) * x_distance] = 1
+                h = np.vstack((h, stab), dtype=np.int8)
+        else:
+            for i in range(x_distance - 1):  # columns
+                stab = np.zeros(n, dtype=np.int8)
+                if i % 2 == 0:
+                    stab[i] = 1
+                    stab[i + 1] = 1
+                else:
+                    stab[i + x_distance * (z_distance - 1)] = 1
+                    stab[i + x_distance * (z_distance - 1) + 1] = 1
+                h = np.vstack((h, stab), dtype=np.int8)
+        return h
