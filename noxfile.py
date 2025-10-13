ebd42ce0488c7b6377791a18f1e229421854b9ef
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-10-14 00:25:21 +0200

    👷 Enable testing on Python 3.14

--- noxfile.py
+++ noxfile.py
@@ -30,9 +30,7 @@ nox.needs_version = ">=2025.10.16"
 nox.options.default_venv_backend = "uv"
 
 
-# TODO(denialhaag): Add 3.14 when all dependencies support it
-#   https://github.com/munich-quantum-toolkit/qecc/issues/464
-PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12", "3.13"]
+PYTHON_ALL_VERSIONS = ["3.10", "3.11", "3.12", "3.13", "3.14"]
 
 if os.environ.get("CI", None):
     nox.options.error_on_missing_interpreters = True
