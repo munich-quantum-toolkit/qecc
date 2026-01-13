d6c0644b49cc1975a52e47bf8124cee5e972db86
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-01-14 00:49:01 +0100

    Fix ruff error

--- src/mqt/qecc/cc_decoder/__init__.py
+++ src/mqt/qecc/cc_decoder/__init__.py
@@ -13,17 +13,7 @@ from ..codes.color_code import ColorCode, LatticeType
 from ..codes.hexagonal_color_code import HexagonalColorCode
 from ..codes.square_octagon_color_code import SquareOctagonColorCode
 from .comparison import tn_decoder
-
-
-def code_from_string(lattice_type: str, distance: int) -> ColorCode:
-    """Construct a color code from a string defining the lattice and a distance."""
-    if lattice_type == LatticeType.HEXAGON:
-        return HexagonalColorCode(distance)
-    if lattice_type == LatticeType.SQUARE_OCTAGON:
-        return SquareOctagonColorCode(distance)
-    msg = f"Unknown lattice type {lattice_type}. Please choose either hexagon or square_octagon."
-    raise ValueError(msg)
-
+from .utils import code_from_string
 
 __all__ = [
     "ColorCode",
