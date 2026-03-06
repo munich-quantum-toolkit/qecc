377010aea4e963e57b78deb9ff55c449e1be8062
Tom Peham <pehamtom@gmx.at>
2026-03-06 21:12:51 +0100

    Fix parallel filter again.

--- src/mqt/qecc/circuit_synthesis/elimination.py
+++ src/mqt/qecc/circuit_synthesis/elimination.py
@@ -486,7 +486,7 @@ class ParallelFilter(OperationFilter):
 
     def has_available_qubits(self) -> bool:
         """Check if there are qubits available for operations."""
-        return self._n_blocked <= self.n_qubits - 1  # two qubits should be free
+        return self._n_blocked < self.n_qubits - 1  # two qubits should be free
 
     def reset(self) -> None:
         """Unblock all qubits."""
