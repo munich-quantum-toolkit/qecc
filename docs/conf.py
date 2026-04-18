789b69ee82dbd477d2be2fd116b4d5955a14d0ce
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2026-04-18 08:26:29 +0200

    Remove dependency on setuptools-scm

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,6 @@
 
 from __future__ import annotations
 
-import warnings
 from importlib import metadata
 from pathlib import Path
 from typing import TYPE_CHECKING
@@ -21,20 +20,10 @@ from pybtex.style.template import field, href
 ROOT = Path(__file__).parent.parent.resolve()
 
 try:
-    from mqt.qecc import __version__ as version
+    version = metadata.version("mqt.qecc")
 except ModuleNotFoundError:
-    try:
-        version = metadata.version("mqt.qecc")
-    except ModuleNotFoundError:
-        msg = (
-            "Package should be installed to produce documentation! "
-            "Assuming a modern git archive was used for version discovery."
-        )
-        warnings.warn(msg, stacklevel=1)
-
-        from setuptools_scm import get_version
-
-        version = get_version(root=str(ROOT), fallback_root=ROOT)
+    msg = "mqt.qecc must be installed to build the documentation"
+    raise ModuleNotFoundError(msg) from None
 
 # Filter git details from version
 release = version.split("+")[0]
