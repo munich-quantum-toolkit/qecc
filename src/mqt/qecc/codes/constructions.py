7df84c04049ff22ab7c9a7ea087c21feed0e760a
Tom Peham <pehamtom@gmx.at>
2026-05-07 15:01:09 +0200

    Docstring.

--- src/mqt/qecc/codes/constructions.py
+++ src/mqt/qecc/codes/constructions.py
@@ -20,7 +20,14 @@ if TYPE_CHECKING:
 
 
 def construct_quantum_hamming_code(r: int) -> CSSCode:
-    """Return the [[2^r-1, 2^r-2r-1, 3]] quantum Hamming code."""
+    """Construct a quantum Hamming code.
+
+    Args:
+        r: Hamming construction parameter.
+
+    Returns:
+        The [[2^r-1, 2^r-2r-1, 3]] quantum Hamming code.
+    """
     h = _hamming_code_checks(r)
     return CSSCode(h, h, 3)
 
