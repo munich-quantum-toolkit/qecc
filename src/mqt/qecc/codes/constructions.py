b9656e8ba7c4b92faf12cce3ab8fe8c2dba053b3
Tom Peham <pehamtom@gmx.at>
2026-05-07 13:54:14 +0200

    Fix typo in `construct_quantum_hamming_code` docstring.

--- src/mqt/qecc/codes/constructions.py
+++ src/mqt/qecc/codes/constructions.py
@@ -20,7 +20,7 @@ if TYPE_CHECKING:
 
 
 def construct_quantum_hamming_code(r: int) -> CSSCode:
-    """Return the [[2^r, 2^r-r-1, 3]] quantum Hamming code."""
+    """Return the [[2^r-1, 2^r-2r-1, 3]] quantum Hamming code."""
     h = _hamming_code_checks(r)
     return CSSCode(h, h, 3)
 
