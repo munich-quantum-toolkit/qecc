4f536412e31b33e3524ce599bc3be5125b5d6107
Yudong Sun <yudong.sun@tum.de>
2026-06-02 16:47:48 +0200

    add num_qubits to repr and add test for repr of XZFaultList

--- src/mqt/qecc/circuit_synthesis/faults.py
+++ src/mqt/qecc/circuit_synthesis/faults.py
@@ -879,5 +879,5 @@ class XZFaultList:
 
     def __repr__(self) -> str:
         """Return a string representation of the XZFaultList."""
-        repr_ = [object.__repr__(self), "X:", repr(self.faults["X"]), "Z:", repr(self.faults["Z"])]
+        repr_ = [object.__repr__(self) + f" num_qubits: {self.num_qubits}", "X:", repr(self.faults["X"]), "Z:", repr(self.faults["Z"])]
         return "\n".join(repr_)
