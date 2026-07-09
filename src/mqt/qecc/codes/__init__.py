d69a12ddcd4210d1ca33f9771208cca70f746501
Maxie Bichmann <maxie.bichmann@tum.de>
2026-07-09 16:10:01 +0200

    📦 add more structure do code constructions

--- src/mqt/qecc/codes/__init__.py
+++ src/mqt/qecc/codes/__init__.py
@@ -10,16 +10,16 @@
 from __future__ import annotations
 
 from .constructions.bb_codes import construct_bb_code
-from .constructions.color_code import ColorCode, LatticeType
+from .constructions.color_codes.color_code import ColorCode, LatticeType
+from .constructions.color_codes.hexagonal_color_code import HexagonalColorCode
+from .constructions.color_codes.square_octagon_color_code import SquareOctagonColorCode
 from .constructions.concatenation import ConcatenatedCode, ConcatenatedCSSCode
 from .constructions.constructions import (
     construct_iceberg_code,
     construct_many_hypercube_code,
     construct_quantum_hamming_code,
 )
-from .constructions.hexagonal_color_code import HexagonalColorCode
 from .constructions.rotated_surface_code import InvalidDistanceError, RotatedSurfaceCode
-from .constructions.square_octagon_color_code import SquareOctagonColorCode
 from .core.css_code import CSSCode, InvalidCSSCodeError
 from .core.stabilizer_code import InvalidStabilizerCodeError, StabilizerCode
 
